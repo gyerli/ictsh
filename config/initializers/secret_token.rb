8f7740cd868def1ad037644e60503a18f5364f76
Gursoy Yerli <gursoy@mint-64.(none)>
2012-10-14 17:08:23 -0500

    ICTSH bootstrap installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ictsh1::Application.config.secret_token = '5407e5be59d71dd63103368338703dedf379cfcb2b92b3a9f113ac4b9b635076be697ba48c51b3fc857be7cd3ce74563ef2da61d1b9cfc5a69c0957ca3604598'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ictsh1::Application.config.session_store :cookie_store, key: '_ictsh-1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ictsh1::Application.config.session_store :active_record_store
